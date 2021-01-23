51bc95cc04dfca1e2a98a46231118e03ffa89ac9
roydensynderen <roydensynderen@gmail.com>
2021-01-23 16:23:06 -0600

    change to Typescript and add checkout functionality

---
+++ pages/result.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+export default function Result() {
+  const router = useRouter()
+
+  const { data, error } = useSWR(
+    router.query.session_id
+      ? `/api/checkout/${router.query.session_id}`
+      : null,
+    url => fetch(url).then(res => res.json())
+  )
+
+  return (
+    <div>
+      <h1>Payment Result</h1>
+
+      <pre>
+        {
+          data 
+            ? JSON.stringify(data)
+            : 'Loading'
+        }
+      </pre>
+    </div>
+  )
+}
